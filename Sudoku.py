a7155fe4fd4b4f0a2bed38a321ff3903a548a76d
sham-404 <shamsujith4@gmail.com>
2025-08-16 22:44:32 +0530

    Made it to solve naked singles

--- Sudoku.py
+++ Sudoku.py
@@ -1,11 +1,28 @@
 from collections import defaultdict
+import time
+import os
+
+start = True
 
 
 class Sudoku:
+    valid_values = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+    def print_board(self, board):
+        global start
+        if start:
+            print("\n\n\n\n\n\n\n\n\n")
+            start = False
+        print("\033[9A", end="")
+
+        for i in board:
+            print(i, flush=True)
+        time.sleep(0.15)
+
     def is_valid_sudoku(self, board):
-        row = defaultdict(set)
-        col = defaultdict(set)
-        box = defaultdict(set)
+        self.row = defaultdict(set)
+        self.col = defaultdict(set)
+        self.box = defaultdict(set)
 
         for r in range(9):
             for c in range(9):
@@ -15,14 +32,56 @@ class Sudoku:
                     continue
 
                 box_no = (r // 3) * 3 + (c // 3)
-                if val in row[r] or val in col[c] or val in box[box_no]:
+                if val in self.row[r] or val in self.col[c] or val in self.box[box_no]:
                     return False
 
-                row[r].add(val)
-                col[c].add(val)
-                box[box_no].add(val)
+                self.row[r].add(val)
+                self.col[c].add(val)
+                self.box[box_no].add(val)
 
         return True
+
+    def naked_singles(self, board):
+        r = 0
+        c = 0
+        found = False
+
+        while r < 9:
+            while c < 9:
+                if test_board[r][c] != 0:
+                    c += 1
+                    continue
+
+                box_no = (r // 3) * 3 + (c // 3)
+                possibility = Sudoku.valid_values - (
+                    self.row[r] | self.col[c] | self.box[box_no]
+                )
+                if len(possibility) == 1:
+                    val = list(possibility)[0]
+                    board[r][c] = val
+
+                    self.row[r].add(val)
+                    self.col[c].add(val)
+                    self.box[box_no].add(val)
+
+                    self.print_board(board)
+
+                    found = True
+                    r = 0
+                    c = 0
+
+                    break
+
+                c += 1
+
+            if found:
+                found = False
+                continue
+
+            r += 1
+            c = 0
+
+        return board
 
 
 s = Sudoku()
@@ -40,3 +99,4 @@ test_board = [
 
 
 print(s.is_valid_sudoku(test_board))
+s.print_board(s.naked_singles(test_board))
