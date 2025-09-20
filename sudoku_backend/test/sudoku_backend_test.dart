3b7f6f1ba801f6cafcc626701e089727244bafb0
sham-404 <shamsujith4@gmail.com>
2025-09-20 14:41:33 +0530

    Failed model

--- sudoku_backend/test/sudoku_backend_test.dart
+++ sudoku_backend/test/sudoku_backend_test.dart
@@ -1,27 +1,42 @@
 //import 'package:sudoku_backend/sudoku_backend.dart';
 //import 'package:test/test.dart';
 
+
+
 bool start = false;
 double speedFac = 2;
-final int rowSize = 9;
-final int colSize = 9;
+int rowSize = 9;
+int colSize = 9;
 
 class Sudoku {
   static Set<int> validValues = {for (int i = 1; i <= rowSize; i++) i};
   final Map<int, Set<int>> rowMap = {};
   final Map<int, Set<int>> colMap = {};
   final Map<int, Set<int>> boxMap = {};
-  final List<(int, int)> empty = [];
+  final List<(int, int)> emptyList = [];
 
   Sudoku() {
     resetMaps();
+  }
+
+  void printBoard(List<List<int>> board, [double x = 0.01]) {}
+
+  void findAllEmpty(List<List<int>> board) {
+    emptyList.clear();
+    for (int i = 0; i < rowSize; i++) {
+      for (int j = 0; j < colSize; j++) {
+        if (board[i][j] == 0) {
+          emptyList.add((i, j));
+        }
+      }
+    }
   }
 
   void resetMaps() {
     rowMap.clear();
     colMap.clear();
     boxMap.clear();
-    empty.clear();
+    emptyList.clear();
     for (int i = 0; i < rowSize; i++) {
       rowMap[i] = <int>{};
       colMap[i] = <int>{};
@@ -29,7 +44,30 @@ class Sudoku {
     }
   }
 
-  void printBoard(List<List<int>> board, [animation=false]) {}
+  bool isValidSudoku(List<List<int>> board) {
+    int val, boxNo;
+    for (int r = 0; r < rowSize; r++) {
+      for (int c = 0; c < colSize; c++) {
+        val = board[r][c];
+        if (val == 0) emptyList.add((r, c));
+
+        boxNo = (r ~/ 3) * 3 + (c ~/ 3);
+
+        // direct duplicate check
+        //if (rowMap[r]!.contains(val) ||
+        //    colMap[c]!.contains(val) ||
+        //    boxMap[boxNo]!.contains(val)) {
+        //  return false;
+        //}
+
+        // then add
+        rowMap[r]!.add(val);
+        colMap[c]!.add(val);
+        boxMap[boxNo]!.add(val);
+      }
+    }
+    return true;
+  }
 
   bool isValidElement(int row, int col, int val) {
     int boxNo = (row ~/ 3) * 3 + (col ~/ 3);
@@ -38,38 +76,6 @@ class Sudoku {
     if (colMap[col]?.contains(val) ?? false) return false;
     if (boxMap[boxNo]?.contains(val) ?? false) return false;
 
-    return true;
-  }
-
-  void findAllEmpty(List<List<int>> board) {
-    empty.clear();
-    for (int i = 0; i < rowSize; i++) {
-      for (int j = 0; j < colSize; j++) {
-        if (board[i][j] == 0) {
-          empty.add((i, j));
-        }
-      }
-    }
-  }
-
-  bool isValidSudoku(List<List<int>> board) {
-    resetMaps();
-    for (int row = 0; row < rowSize; row++) {
-      for (int col = 0; col < colSize; col++) {
-        final int val = board[row][col];
-
-        if (val == 0) continue;
-
-        if (!isValidElement(row, col, val)) {
-          return false;
-        }
-
-        final boxNo = (row ~/ 3) * 3 + (col ~/ 3);
-        rowMap[row]!.add(val);
-        colMap[col]!.add(val);
-        boxMap[boxNo]!.add(val);
-      }
-    }
     return true;
   }
 
@@ -126,57 +132,85 @@ class Sudoku {
     }
   }
 
-
-  void solver(List<List<int>> board) {
+  void solver(List<List<int>> board, [bool animation = false]) {
+    start = true;
+    resetMaps();
     if (!isValidSudoku(board)) {
-      print("Not a valid Sudoku board");
+      print("This is not a valid sudoku!");
       return;
     }
 
-    nakedSingles(board);
+    nakedSingles(board, animation);
 
-    findAllEmpty(board);
-    print("It is a valid Sudoku!\n");
+    //findAllEmpty(board);
+    int row, col, boxNo;
+    print(emptyList);
 
-    bool backtrack([int idx = 0]) {
-      if (idx == empty.length) return true;
+    bool backtracking([int idx = 0]) {
+      if (idx == emptyList.length) {
+        return true;
+      }
 
-      final (row, col) = empty[idx];
-      final int boxNo = (row ~/ 3) * 3 + (col ~/ 3);
+      (row, col) = emptyList[idx];
+      boxNo = (row ~/ 3) * 3 + (col ~/ 3);
 
-      for (var val = 1; val <= 9; val++) {
+      for (int val = 1; val <= rowSize; val++) {
         if (isValidElement(row, col, val)) {
           board[row][col] = val;
+
+          print("$row, $col, $val");
+
+          if (animation) {
+            for (var i in board) {
+              print(i);
+            }
+            print('');
+          }
+
           rowMap[row]!.add(val);
           colMap[col]!.add(val);
           boxMap[boxNo]!.add(val);
 
-          if (backtrack(idx + 1)) return true;
+          print(rowMap);
+          print(colMap);
+          print(boxMap);
+
+          if (backtracking(idx + 1)) {
+            return true;
+          }
 
           board[row][col] = 0;
+          print("After removing $row, $col, $val");
+
           rowMap[row]!.remove(val);
           colMap[col]!.remove(val);
           boxMap[boxNo]!.remove(val);
+
+          print(rowMap);
+          print(colMap);
+          print(boxMap);
         }
       }
+
       return false;
     }
 
-    backtrack();
+    backtracking();
   }
 }
 
 void main() {
-  final List<List<int>> board = [
-    for (int i = 0; i < 9; i++) [for (int i = 0; i < 9; i++) 0],
+  List<List<int>> testBoard1 = [
+    for (int i = 0; i < 9; i++) [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ];
 
-  final Sudoku solver = Sudoku();
-  print(solver.isValidSudoku(board));
-  print(solver.empty);
-  solver.solver(board);
-
-  for (final row in board) {
-    print(row);
+  Sudoku s = Sudoku();
+  //s.findAllEmpty(testBoard);
+  //print(s.emptyList);
+  //print(s.isValidSudoku(testBoard1));
+  //print(s.emptyList);
+  s.solver(testBoard1, true);
+  for (var i in testBoard1) {
+    print(i);
   }
 }
