cc1633380683e93e89ff2a997214d5a038e97472
sham-404 <shamsujith4@gmail.com>
2025-09-21 18:14:45 +0530

    Small changes

--- sudoku_backend/example/sudoku_backend_example.dart
+++ sudoku_backend/example/sudoku_backend_example.dart
@@ -1,6 +1,6 @@
-import 'package:sudoku_backend/sudoku_backend.dart';
+// import 'package:sudoku_backend/sudoku_backend.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
+// void main() {
+//   var awesome = Awesome();
+//   print('awesome: ${awesome.isAwesome}');
+// }
